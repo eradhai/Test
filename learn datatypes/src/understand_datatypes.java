c69a0657fac73991b76a78f393a389c5d932e130
Radhai <Radhai@DESKTOP-BVII0Q0.lan>
2022-12-02 00:39:31 +0530

    push

---
+++ learn datatypes/src/understand_datatypes.java
@@ -0,0 +1,29 @@
+
+public class understand_datatypes {
+	public static void main(String[] args) {
+	byte bytevalue = 30;
+	short shortvalue = 100;
+	int intvalue = 1000;
+	long longvalue = 5465657668l;
+	float floatValue = 50.2f;
+	double doubleValue=150.35d;
+	char CharValue = 'S';
+	boolean boolvalue = true;
+	String stringValue = "Jeyam";
+	String stringAlphaNum = "Jeyam123";
+	String stringSpecialChar = "Jeyam&123!%%%%%%%%";
+	System.out.println("The Value of Byte is: "+bytevalue);
+	System.out.println("The Value of Short is: "+shortvalue);
+	System.out.println("The Value of Int is: "+intvalue);
+	System.out.println("The Value of Long is: "+longvalue);
+	System.out.println("The Value of Float is: "+floatValue);
+	System.out.println("The Value of Double is: "+doubleValue);
+	System.out.println("The Value of Char is: "+CharValue);
+	System.out.println("The Value of Boolean is: "+boolvalue);
+	System.out.println("The Value of StringValue is: "+stringValue);
+	System.out.println("The Value of StringAlphaNum is: "+stringAlphaNum);
+	System.out.println("The Value of StringSpecialChar is: "+stringSpecialChar);
+	
+	}	
+	
+}
